fb34d41a83168b7bb958bc1e7310c8a7facfb94a
APO <igwefredrick12@gmail.com>
2024-02-23 12:23:58 +0100

    my first commit

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 
 """Contains the flask app"""
+from flask import Flask, request
+from helper import generate_qr_code 
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    if request.method == "GET":
+        print("Get")
+        return ({"message": "GET"})
+
+@app.route("/qr_code", methods=["POST"])
+def qr_code():
+   data = request.get_json()
+   generate_qr_code(data)
+       
