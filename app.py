f99915d157f267dfe81b488ff2425fdfb7071e45
APO <igwefredrick12@gmail.com>
2024-02-24 10:52:46 +0100

    completed creating the postgresql server connection in database.py (Task 6)

--- app.py
+++ app.py
@@ -10,35 +10,3 @@ The '/save_to_db' endpoint takes a JSON payload and saves it to the database aft
 #!/usr/bin/env python
 
 """Contains the flask app"""
-from flask import Flask, request
-from models import helper 
-# The helper file contains our generate uuid and qr code generator
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    if request.method == "GET":
-        print("Get")
-        return ({"message": "GET"})
-
-@app.route("/qr_code", methods=["POST"])
-def qr_code():
-    try:
-        data = request.get_json()
-        filename = helper.generate_qr_code(**data)
-        return ({"message": "QR code generated", "filename": filename})
-    
-    except Exception as e:
-        return e
-
-@app.route("/save_to_db", methods=["POST"])
-def save_to_db(data):
-    try:
-        data = request.get_json()
-        filename = helper.generate_qr_code(**data)
-        return ({"message": "QR code generated", "filename": filename})
-    
-    except Exception as e:
-        return e
--- database.py
+++ database.py
@@ -1,38 +1,26 @@
-"""
-database.py - Contains the database connection and communication functions.
-
-Exports the connect() function to initialize a connection to the PostgreSQL database.
-Also contains __main__ block to load config and connect if run directly.
-"""
 #!/usr/bin/env python 
 """Contains the database connection and all communication with the database"""
 import psycopg2
-from config import load_config
+
+params = {"host" :"localhost",
+          "port" : 5432,
+        "database" : "qrcodes",
+        "user" :"postgres",
+        "password": "****"}
+
 
 def connect(config):
-    """ Connect to the PostgreSQL database server and create table qrcodes2 """
+    """
+    Connects to a PostgreSQL database using the provided configuration.
+
+    Prints a message indicating whether the connection was successful.
+    Returns the database connection object on success."""
     try:
-        # connecting to the PostgreSQL server
+        # connecting to the PostgreSQL server        
         with psycopg2.connect(**config) as conn:
             print('Connected to the PostgreSQL server.')
-            query = "CREATE TABLE qrcodes2 if not exist (id SERIAL PRIMARY KEY, uuid VARCHAR(36), qrdata VARCHAR(500));"
-            with conn.cursor() as cur:
-                cur.execute(query)
             return conn
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
-#TODO make function work
-def save_qr_data(**data):
-    pass
-    
-    
-    
-    
-
-# If this module is run directly, load the configuration and connect to the database
-if __name__ == '__main__':
-    config = load_config()
-    connect(config)
-    
-    
+connect(params)
