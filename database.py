337bfa941f7db2e57d1331eb6dcc95238da2d94f
APO <igwefredrick12@gmail.com>
2024-02-24 05:33:02 +0100

    created first connection

--- database.py
+++ database.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python 
 """Contains the database connection and all communication with the database"""
 import psycopg2
+from config import load_config
 
-conn = psycopg2.connect(host = "localhost",database ="postgresql", user="postgres", password="HisGrace@4", port="5432")
+def connect(config):
+    """ Connect to the PostgreSQL database server """
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server.')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    config = load_config()
+    connect(config)
